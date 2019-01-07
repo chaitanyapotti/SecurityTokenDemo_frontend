b3d3fb5b26b663be128eaea6073dc4b662cb9cdc
unknown <kvppradeep999@gmail.com>
2019-01-07 18:08:49 +0530

    modal

---
+++ src/components/common/AlertModal/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { CUIModal, CUIModalActions, CUIModalContent } from "../../../helpers/material-ui";
+import LoadingButton from "../LoadingButton";
+
+const AlertModal = props => {
+  const { open, children, handleClose } = props || {};
+  return (
+    <div>
+      <CUIModal open={open}>
+        <CUIModalContent className="ModalContent">{children}</CUIModalContent>
+        <CUIModalActions className="ModalActions">
+          <div className="hli">
+            <LoadingButton onClick={handleClose}>Close</LoadingButton>
+          </div>
+        </CUIModalActions>
+      </CUIModal>
+    </div>
+  );
+};
+
+export default AlertModal;
