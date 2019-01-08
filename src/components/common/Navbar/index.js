3e2370322bb31a8acf9b5321ea26f7101432acf3
unknown <kvppradeep999@gmail.com>
2019-01-08 13:04:07 +0530

    navbar changes

--- src/components/common/Navbar/index.js
+++ src/components/common/Navbar/index.js
@@ -1,25 +1,28 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
 import { Link } from "react-router-dom";
 
-class Navbar extends Component {
+class Navbar extends PureComponent {
   render() {
     return (
-      <nav className="navbar navbar-expand-sm navbar-dark bg-primary mb-4 fixed-top">
+      <nav className="navbar navbar-expand-sm navbar-light bg-primary mb-4 fixed-top">
         <div className="container">
-          <img src="/assets/TWO12BlkOrange.png" alt="whitelist checked" width="120" height="35" />
+          <img src="/assets/TWO12BlkWht.png" alt="whitelist checked" width="120" height="35" />
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
             <span className="navbar-toggler-icon" />
           </button>
-
-          <div className="collapse navbar-collapse" id="mobile-nav">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to="/">
-                  Login
-                </Link>
-              </li>
-            </ul>
-          </div>
+          {localStorage.jwtToken ? (
+            <div className="collapse navbar-collapse" id="mobile-nav">
+              <ul className="nav nav-pills ml-auto">
+                <li className="nav-item">
+                  <Link className="nav-link active" to="/">
+                    Logout
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          ) : (
+            <div />
+          )}
         </div>
       </nav>
     );
