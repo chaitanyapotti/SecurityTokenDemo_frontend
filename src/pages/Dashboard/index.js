5e4f1459c2fa985096e7af8d7a1e047808cae462
chaitanyapotti <chaitanya@electus.network>
2019-02-03 17:54:34 +0530

    used code splitting

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,12 +1,13 @@
-import React, { Component } from "react";
+import React, { Component, Suspense } from "react";
 import { Button } from "semantic-ui-react";
 import { connect } from "react-redux";
 import Proptypes from "prop-types";
 import { logoutUserAction } from "../../actions/authActions";
-import InvestorDashboard from "../../containers/InvestorDashboard";
-import MarketMakerDashboard from "../../containers/MarketMakerDashboard";
-import BrokerDealerDashboard from "../../containers/BrokerDealerDashboard";
 import { getPriceHistory } from "../../actions/priceHistoryActions";
+
+const InvestorDashboard = React.lazy(() => import("../../containers/InvestorDashboard"));
+const MarketMakerDashboard = React.lazy(() => import("../../containers/MarketMakerDashboard"));
+const BrokerDealerDashboard = React.lazy(() => import("../../containers/BrokerDealerDashboard"));
 
 class Dashboard extends Component {
   onLogoutClick = e => {
@@ -28,13 +29,25 @@ class Dashboard extends Component {
     const { history } = this.props || {};
     const { role } = JSON.parse(localStorage.getItem("user_data")) || {};
     if (role === "INVESTOR") {
-      return <InvestorDashboard history={history} />;
+      return (
+        <Suspense fallback={<div />}>
+          <InvestorDashboard history={history} />
+        </Suspense>
+      );
     }
     if (role === "MARKET_MAKER") {
-      return <MarketMakerDashboard history={history} />;
+      return (
+        <Suspense fallback={<div />}>
+          <MarketMakerDashboard history={history} />
+        </Suspense>
+      );
     }
     if (role === "BROKER_DEALER") {
-      return <BrokerDealerDashboard history={history} />;
+      return (
+        <Suspense fallback={<div />}>
+          <BrokerDealerDashboard history={history} />
+        </Suspense>
+      );
     }
     return <Button onClick={this.onLogoutClick}>Logout</Button>;
   }
