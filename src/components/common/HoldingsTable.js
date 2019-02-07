66c6062af4e64d45a08ad6bbb1fccf85c678a0e8
chaitanyapotti <chaitanya@electus.network>
2019-02-07 15:42:18 +0530

    updated packages and reverted changes

--- src/components/common/HoldingsTable.js
+++ src/components/common/HoldingsTable.js
@@ -1,51 +1,48 @@
 import React, { PureComponent } from "react";
-import { Table, TableBody, TableCell, TableHead, TableRow, Paper } from "@material-ui/core";
-import { formatCurrencyNumber, formatMoney, getEtherScanAddressLink } from "../../helpers/numberHelpers";
+import { Table } from "semantic-ui-react";
+import { formatCurrencyNumber, formatMoney, getEtherScanAddressLink, significantDigits } from "../../helpers/numberHelpers";
 import config from "../../config";
 
 class HoldingsTable extends PureComponent {
   render() {
     const { tokenBalance, currentPortfolioValue } = this.props || {};
     return (
-      <Paper style={{ marginBottom: "20px" }} className="card-brdr">
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Token Name</TableCell>
-              <TableCell>Token Count</TableCell>
-              <TableCell>Invested Value($)</TableCell>
-              <TableCell>Current Value($)</TableCell>
-              <TableCell>Change</TableCell>
-              <TableCell>Token Price</TableCell>
-              <TableCell>EtherScan</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {Object.keys(tokenBalance).map(key => (
-              <TableRow key={key}>
-                <TableCell>{config.tokens[key].name}</TableCell>
-                <TableCell>{formatCurrencyNumber(tokenBalance[key].balance, 0)}</TableCell>
-                <TableCell>{formatMoney(tokenBalance[key].dollarValue, 0)}</TableCell>
-                <TableCell>{formatMoney(currentPortfolioValue[key], 0)}</TableCell>
-                <TableCell>
-                  {`+${formatMoney(currentPortfolioValue[key] - tokenBalance[key].dollarValue, 0)}(+${Math.round(
-                    ((currentPortfolioValue[key] - tokenBalance[key].dollarValue) * 100) / tokenBalance[key].dollarValue,
-                    2
-                  )}%)`}
-                </TableCell>
-                <TableCell>{formatMoney(currentPortfolioValue[key] / tokenBalance[key].balance || 0, 2)}</TableCell>
-                <TableCell>
-                  <span>
-                    <a href={getEtherScanAddressLink(config.tokens[key].address, "rinkeby")} target="_blank" rel="noopener noreferrer">
-                      View on Blockchain
-                    </a>
-                  </span>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </Paper>
+      <Table celled>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Token Name</Table.HeaderCell>
+            <Table.HeaderCell>Token Count</Table.HeaderCell>
+            <Table.HeaderCell>Invested Value($)</Table.HeaderCell>
+            <Table.HeaderCell>Current Value($)</Table.HeaderCell>
+            <Table.HeaderCell>Change</Table.HeaderCell>
+            <Table.HeaderCell>EtherScan</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {Object.keys(tokenBalance).map(key => (
+            <Table.Row key={key}>
+              <Table.Cell>{config.tokens[key].name}</Table.Cell>
+              <Table.Cell>{formatCurrencyNumber(tokenBalance[key].balance, 0)}</Table.Cell>
+              <Table.Cell>{formatMoney(tokenBalance[key].dollarValue, 0)}</Table.Cell>
+              <Table.Cell>{formatMoney(currentPortfolioValue[key], 0)}</Table.Cell>
+              <Table.Cell>{significantDigits(tokenBalance[key].dollarValue / tokenBalance[key].balance || 0)}</Table.Cell>
+              <Table.Cell>
+                {`+${formatMoney(currentPortfolioValue[key] - tokenBalance[key].dollarValue, 0)}(+${Math.round(
+                  ((currentPortfolioValue[key] - tokenBalance[key].dollarValue) * 100) / tokenBalance[key].dollarValue,
+                  2
+                )}%)`}
+              </Table.Cell>
+              <Table.Cell>
+                <span>
+                  <a href={getEtherScanAddressLink(config.tokens[key].address, "rinkeby")} target="_blank" rel="noopener noreferrer">
+                    View on Blockchain
+                  </a>
+                </span>
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
     );
   }
 }
