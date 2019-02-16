c5ba48af1b0bbc6138f8492b150630706ba0649c
chaitanyapotti <chaitanya@electus.network>
2019-02-16 12:26:58 +0800

    removed unnecessary stuff

--- src/components/CustomMUI/CustomButton/index.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-import { Button } from "@material-ui/core";
-
-export const CustomButton = props => (
-  <Button className="btn bg--primary txt-p-vault txt-dddbld text--white" {...props}>
-    <div style={{ textTransform: "initial", fontWeight: "bold" }}>{props.children}</div>
-  </Button>
-);
