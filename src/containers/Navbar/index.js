60c9170b2ad9636e69677de2e5496cf6934a9e5c
chaitanyapotti <chaitanya@electus.network>
2019-03-05 18:01:30 +0530

    App bar re-design

--- src/containers/Navbar/index.js
+++ src/containers/Navbar/index.js
@@ -1,39 +1,73 @@
 import React, { PureComponent } from "react";
 import { connect } from "react-redux";
 import Proptypes from "prop-types";
-import { Button, AppBar } from "@material-ui/core";
+import { Button, AppBar, IconButton, Menu, MenuItem, Toolbar } from "@material-ui/core";
+import { AccountCircle } from "@material-ui/icons";
 import { withRouter } from "react-router-dom";
 import { Grid, Row, Col } from "../../helpers/react-flexbox-grid";
 import { logoutUserAction } from "../../actions/authActions";
 
 class Navbar extends PureComponent {
+  state = {
+    menuOpen: false,
+    anchorEl: null
+  };
+
   onLogoutClick = e => {
     const { logoutUserAction: logoutUser } = this.props;
     const { history } = this.props || {};
+    this.handleClose();
     logoutUser(history);
+  };
+
+  handleClose = () => {
+    this.setState({ menuOpen: false });
+  };
+
+  handleOpen = event => {
+    this.setState({ menuOpen: true, anchorEl: event.currentTarget });
+  };
+
+  onProfileClicked = e => {
+    const { history } = this.props || {};
+    this.handleClose();
+    history.push("/profile");
   };
 
   render() {
     const { isAuthenticated } = this.props || {};
+    const { menuOpen, anchorEl } = this.state;
     return (
       <AppBar className="bg-test">
-        <Grid>
-          <Row>
-            <Col lg={10} sm={6}>
-              <img src="/assets/TWO12BlkWht.png" alt="whitelist checked" width="105" height="45" />
-            </Col>
-            <Col lg={2} sm={6}>
-              <span>
-                {isAuthenticated && (
-                  <Button className="btn bg--danger txt-p-vault txt-dddbld text--white push-half-h--top" onClick={this.onLogoutClick}>
-                    Logout
-                  </Button>
-                )}
-              </span>
-              <span className="push--left push--top">v0.9</span>
-            </Col>
-          </Row>
-        </Grid>
+        <Toolbar>
+          <img src="/assets/TWO12BlkWht.png" alt="whitelist checked" width="105" height="45" />
+          <div style={{ flexGrow: 1 }} />
+          {isAuthenticated && (
+            <div>
+              <IconButton onClick={this.handleOpen} color="inherit" aria-owns={menuOpen ? "menu-appbar" : undefined} aria-haspopup="true">
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right"
+                }}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right"
+                }}
+                open={menuOpen}
+                onClose={this.handleClose}
+              >
+                <MenuItem onClick={this.onProfileClicked}>Profile</MenuItem>
+                <MenuItem onClick={this.onLogoutClick}>Logout</MenuItem>
+              </Menu>
+              <span className="push--left">v0.9</span>
+            </div>
+          )}
+        </Toolbar>
       </AppBar>
     );
   }
