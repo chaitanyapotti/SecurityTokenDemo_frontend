475126ef34b3ffc3a89107e7744d564e04fdb5f2
chaitanyapotti <chaitanya@electus.network>
2019-03-05 18:14:27 +0530

    comment

--- src/containers/Navbar/index.js
+++ src/containers/Navbar/index.js
@@ -1,10 +1,9 @@
 import React, { PureComponent } from "react";
 import { connect } from "react-redux";
 import Proptypes from "prop-types";
-import { Button, AppBar, IconButton, Menu, MenuItem, Toolbar } from "@material-ui/core";
+import { AppBar, IconButton, Menu, MenuItem, Toolbar } from "@material-ui/core";
 import { AccountCircle } from "@material-ui/icons";
 import { withRouter } from "react-router-dom";
-import { Grid, Row, Col } from "../../helpers/react-flexbox-grid";
 import { logoutUserAction } from "../../actions/authActions";
 
 class Navbar extends PureComponent {
@@ -38,37 +37,39 @@ class Navbar extends PureComponent {
     const { isAuthenticated } = this.props || {};
     const { menuOpen, anchorEl } = this.state;
     return (
-      <AppBar className="bg-test">
-        <Toolbar>
-          <img src="/assets/TWO12BlkWht.png" alt="whitelist checked" width="105" height="45" />
-          <div style={{ flexGrow: 1 }} />
-          {isAuthenticated && (
-            <div>
-              <IconButton onClick={this.handleOpen} color="inherit" aria-owns={menuOpen ? "menu-appbar" : undefined} aria-haspopup="true">
-                <AccountCircle />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "right"
-                }}
-                anchorOrigin={{
-                  vertical: "top",
-                  horizontal: "right"
-                }}
-                open={menuOpen}
-                onClose={this.handleClose}
-              >
-                <MenuItem onClick={this.onProfileClicked}>Profile</MenuItem>
-                <MenuItem onClick={this.onLogoutClick}>Logout</MenuItem>
-              </Menu>
-              <span className="push--left">v0.9</span>
-            </div>
-          )}
-        </Toolbar>
-      </AppBar>
+      <div style={{ flexGrow: 1 }}>
+        <AppBar className="bg-test">
+          <Toolbar>
+            <img src="/assets/TWO12BlkWht.png" alt="whitelist checked" width="105" height="45" />
+            <div style={{ flexGrow: 1 }} />
+            {isAuthenticated && (
+              <div>
+                <IconButton onClick={this.handleOpen} color="inherit" aria-owns={menuOpen ? "menu-appbar" : undefined} aria-haspopup="true">
+                  <AccountCircle />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorEl}
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right"
+                  }}
+                  anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right"
+                  }}
+                  open={menuOpen}
+                  onClose={this.handleClose}
+                >
+                  <MenuItem onClick={this.onProfileClicked}>Profile</MenuItem>
+                  <MenuItem onClick={this.onLogoutClick}>Logout</MenuItem>
+                </Menu>
+                <span className="push--left">v0.9</span>
+              </div>
+            )}
+          </Toolbar>
+        </AppBar>
+      </div>
     );
   }
 }
