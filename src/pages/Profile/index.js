79da971bca695a92935e4a159d04f7f04cb353fd
chaitanyapotti <chaitanya@electus.network>
2019-03-06 22:54:15 +0530

    fix

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -24,7 +24,7 @@ class Profile extends PureComponent {
   componentDidMount() {
     const { isAuthenticated, history } = this.props || {};
     if (!isAuthenticated) {
-      history.push("/")
+      history.push("/");
     }
   }
 
